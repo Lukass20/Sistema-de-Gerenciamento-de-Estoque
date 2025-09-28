2134d5c7e004e67f0c848dfcb6766381d9e70471
Lukass20 <lukassouzavieira15@gmail.com>
2025-09-27 22:09:38 -0300

    atualização

--- Servidor/index.js
+++ Servidor/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Programando21@',
+  password: '',
   database: 'garritano_database'
 });
 
